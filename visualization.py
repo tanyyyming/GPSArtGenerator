801650a879bbcc9ff5f007308eec3cf30436964e
Tan Yiming <tanyiming.nus@gmail.com>
2025-01-18 17:12:11 +0800

    Refactor code to have better abstraction

--- visualization.py
+++ visualization.py
@@ -7,16 +7,32 @@ from OSM import get_roads, construct_graph, construct_node_coordinates, get_clos
 from shape_processor import image_to_coordinates
 
 bbox = [45.44, -122.63, 45.50, -122.57]
-roads = get_roads(bbox)
-graph = construct_graph(roads)
-nodes = construct_node_coordinates(roads)
-
-image_filename = "images/pi.jpg"
+image_filename = "images/infinity.png"
 image_name = re.search(r"([^\/]+)(?=\.[^.]+$)", image_filename).group(1)
-image_coords = image_to_coordinates(image_filename, bbox)
+gpx_filename = f"routes/{image_name}.gpx"
+map_filename = f"routes/{image_name}.html"
+
+def construct_route(image_filename: str, bbox: list[float]) -> list[tuple[float, float]]:
+    # construct the graph and node coordinates
+    roads = get_roads(bbox)
+    graph = construct_graph(roads)
+    nodes = construct_node_coordinates(roads)
+
+    # convert the image points to coordinates within the bounding box
+    image_coords = image_to_coordinates(image_filename, bbox)
+
+    route = []
+
+    for s, e in zip(image_coords[:-1], image_coords[1:]):
+        start_node = get_closest_node(s, nodes)
+        end_node = get_closest_node(e, nodes)
+        path = dijkstra(graph, nodes, start_node, end_node)
+        route.extend(path)
+
+    return route
 
 
-def create_gpx_file(coords, filename):
+def create_gpx_file(route: list[tuple[float, float]], filename: str):
     gpx = gpxpy.gpx.GPX()
 
     # Create first track in our GPX:
@@ -27,7 +43,7 @@ def create_gpx_file(coords, filename):
     gpx_segment = gpxpy.gpx.GPXTrackSegment()
     gpx_track.segments.append(gpx_segment)
 
-    for coord in coords:
+    for coord in route:
         gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(coord[0], coord[1]))
 
     directory = os.path.dirname(filename)
@@ -36,32 +52,18 @@ def create_gpx_file(coords, filename):
         
     with open(filename, "w") as f:
         f.write(gpx.to_xml())
+    
 
-full_route = []
-# Create a map centered at the given latitude and longitude
-m = folium.Map(location=[45.467, -122.637], zoom_start=15)
+def create_html_map(route: list[tuple[float, float]], bbox: list[float], filename: str):
+    # Create a map centered at the middle of the bounding box
+    m = folium.Map(location=[(bbox[0] + bbox[2]) / 2, (bbox[1] + bbox[3]) / 2], zoom_start=13)
+    folium.PolyLine(route).add_to(m)
+    m.save(filename)
 
-for s, e in zip(image_coords[:-1], image_coords[1:]):
-    start_node = get_closest_node(s, nodes)
-    end_node = get_closest_node(e, nodes)
-    path = dijkstra(graph, nodes, start_node, end_node)
-    folium.CircleMarker(
-        location=nodes[start_node],
-        radius=10,
-        color="red",
-        fill=True,
-    ).add_to(m)
-    folium.CircleMarker(
-        location=nodes[end_node],
-        radius=10,
-        color="red",
-        fill=True,
-    ).add_to(m)
-    folium.PolyLine(path).add_to(m)
-    full_route.extend(path)
 
-gpx_filename = f"routes/{image_name}.gpx"
-create_gpx_file(full_route, gpx_filename)
+route = construct_route(image_filename, bbox)
+create_gpx_file(route, gpx_filename)
+create_html_map(route, bbox, map_filename)
 
 # start_node = 1710336791
 # end_node = 454072437
@@ -90,5 +92,3 @@ create_gpx_file(full_route, gpx_filename)
 # folium.PolyLine([start_coords, end_coords], color="red").add_to(m)
 
 # folium.PolyLine(image_coords).add_to(m)
-
-m.save(f"routes/{image_name}.html")
