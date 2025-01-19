794312c316600fbd933f5e1f6b9e1925a6e6bb58
Tan Yiming <tanyiming.nus@gmail.com>
2025-01-19 12:10:43 +0800

    Refine api endpoint

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import numpy as np
 from OSM import get_roads, construct_graph, construct_node_coordinates, get_closest_node
 from shape_processor import line_trace_to_coordinates
 from dijkstra import dijkstra
@@ -6,7 +7,7 @@ from dijkstra import dijkstra
 def points_to_route(
     points: list[tuple[float]],
     image_dimension: tuple[int, int],
-    bbox: list[float] = [45.44, -122.63, 45.50, -122.57],
+    bbox: list[float],
 ) -> list[tuple[float, float]]:
     """
     Given a list of points, return a route that connects them on map.
@@ -17,7 +18,7 @@ def points_to_route(
     graph = construct_graph(roads)
     nodes = construct_node_coordinates(roads)
 
-    image_coords = line_trace_to_coordinates(points, image_dimension, bbox)
+    image_coords = line_trace_to_coordinates(np.array(points), image_dimension, bbox)
 
     route = []
 
--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import api
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route("/image-to-route", methods=["POST"])
-def image_to_route():
+@app.route("/")
+def echo():
+    return "Hi!", 200
+
+
+@app.route("/api/points_to_route", methods=["POST"])
+def points_to_route():
     try:
         data = request.json
 
@@ -13,6 +20,7 @@ def image_to_route():
             return jsonify({"error": "Invalid request"}), 400
 
         dimension = (data["height"], data["width"])
+        bbox = data["bbox"] if "bbox" in data else [45.44, -122.63, 45.50, -122.57]
 
         routes = []
 
@@ -20,7 +28,7 @@ def image_to_route():
             # points are in the form [{x: 1, y: 2}, {x: 3, y: 4}]
             # convert the points to a list of tuples
             points = [(point["x"], point["y"]) for point in line["points"]]
-            route = api.points_to_route(points, dimension)
+            route = api.points_to_route(points, dimension, bbox)
             routes.append(route)
 
         return jsonify({"routes": routes}), 200
@@ -30,4 +38,4 @@ def image_to_route():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8888)
